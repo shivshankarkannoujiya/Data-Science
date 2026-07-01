630cf312c163a5d197d18e457c5c8ea99a5d69a3
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2026-07-01 15:40:54 +0530

    Fix comparison logic in ANOVA testing for caffeine content analysis

--- 01statistical_foundation/10Anova_testing.py
+++ 01statistical_foundation/10Anova_testing.py
@@ -4,9 +4,11 @@ group_A = [105, 110, 98, 107, 103]
 group_B = [130, 125, 132, 128, 129]
 group_C = [120, 115, 117, 119, 121]
 
+alpha = 0.05
+
 f_value, p_value = stats.f_oneway(group_A, group_B, group_C)
 
-if f_value < p_value:
+if alpha < p_value:
     print(
         f"Fail to rejct the null Hypothesis, meaning there is no significat different in the average caffience content in the above three energy groups"
     )
